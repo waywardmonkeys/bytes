8b0da78cd13ecc037dfd49ca4aea44ac45d52862
Travis CI User <travis@example.org>
2019-06-11 19:01:48 +0000

    Documentation for tokio-rs/bytes@dea868a

---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["byteorder"] = {"name":"","files":["io.rs","lib.rs"]};
+sourcesIndex["bytes"] = {"name":"","dirs":[{"name":"buf","files":["buf.rs","buf_mut.rs","chain.rs","from_buf.rs","into_buf.rs","iter.rs","mod.rs","reader.rs","take.rs","vec_deque.rs","writer.rs"]}],"files":["bytes.rs","debug.rs","lib.rs"]};
+createSourceSidebar();
